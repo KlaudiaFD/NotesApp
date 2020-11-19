a11cf662bd038e7952c9dcf0b634384065b7dbf5
Klaudia <oska456@wp.pl>
2020-11-19 17:49:10 +0100

    notesApp

--- main.js
+++ main.js
@@ -1,52 +1,82 @@
-let $notesArea;
-let $add;
-let $newNotes;
-let $notesList;
-let $idNumber=0;
-let $allNotes;
+const btnSave=document.querySelector('#btnSave');
+let notes="";
+init();
 
+function init(){
+    let out="";
+    let noteArray=JSON.parse(localStorage.getItem('noteData'));
+    
+    if(noteArray != null && noteArray != ""){
+        noteArray= JSON.parse(localStorage.getItem('noteData'));
+        
+        for (let x=0; x < noteArray.length; x++){
+            out += "<option value=" +x + ">";
+            out += noteArray[x].title;
+            out += "</option>";
+            
+            document.querySelector('#noteMaster').innerHTML=out;
+        }
+        
+        document.querySelector('#btnWrite').addEventListener('click', function(e){
+            writeNote();
+        });
+        
+        document.querySelector('#noteMaster').addEventListener('click', function(e){
+            displayNote(e.target.value);
+        });
+        
+        readNotes();
+    } else{
+        
+        writeNote();
+    }
+}
 
-
-
-const main=()=>{
-    prepareDOMElements();
-    prepareDOMEvents();
+function writeNote(){
+    document.querySelector('#read').style.display="none";
+    document.querySelector('#write').style.display="block";
+    document.querySelector('#noteTitle').value="";
+    document.querySelector('#noteBody').value="";
 };
 
+function readNotes(){
+    document.querySelector('#read').style.display="block";
+    document.querySelector('#write').style.display="none";
+};
 
-const prepareDOMElements=()=>{
-    $notesArea=document.querySelector('.notesArea');
-    $add=document.querySelector('add');
-    $newNotes=document.querySelector('.newNotes');
+function displayNote(note){
+    let noteArray= JSON.parse(localStorage.getItem('noteData'));
+    let out="<h2>" + noteArray[note].title + "</h2>";
+    out += "<h4>Date: " + new Date(noteArray[note].date).toDateString() + "</h4>";
+    out += "<p>" + noteArray[note].body + "</p>";
+    out += "<button id='btnDelete'> Delete </button>" 
+    document.querySelector('#noteDisplay').innerHTML=out;
+    document.querySelector('#btnDelete').onclick=function(){
+        noteArray.splice(note,1);
+        localStorage.setItem('noteData', JSON.stringify(noteArray));
+        init();
+    }
+};
 
-    $edit=document.querySelector('.edit');
+btnSave.onclick=function(){
+    const noteDate=new Date();
+    const noteTitle= document.querySelector('#noteTitle').value;
+    const noteBody= document.querySelector('#noteBody').value;
+    const theNote= new Note(noteDate, noteTitle, noteBody);
+    saveNotes(theNote);
+};
 
-}
+function saveNotes(note){
+    let noteArray= JSON.parse(localStorage.getItem('noteData'));
+    if (noteArray == null){
 
-const prepareDOMEvents=()=>{
-    $add.addEventListener('click', addNewNotes);
-}
-
-
-const addNewNotes=()=>{
-    if($notesArea.value !==''){
-        $idNumber++;
-        $newNotes=document.createElement('container');
-        $newNotes.innerText=$notesArea.value;
-        $newNotes.setAttribute('id',`container-${$idNumber}`);
-        $newNotes.appendChild($newNotes);
-
-        $notesArea.value='';
+        noteArray= new Array();
+        noteArray.push(note);
 
     }else{
-        alert=" Nie wpisano żadnej treści."
+        noteArray.push(note)
     }
+    localStorage.setItem('noteData', JSON.stringify(noteArray));
+    readNotes();
+    init();
 };
-
-const enterCheck=()=>{
-    if(event.keyCode===13){
-        addNewNotes();
-    }
-};
-
-const 
